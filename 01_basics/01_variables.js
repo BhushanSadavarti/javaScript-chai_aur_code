f07259f1d815a9cc2e2509ab3d86c46dbb8d615e
Bhushan Sadavarti <156838484+BhushanSadavarti@users.noreply.github.com>
2025-08-21 17:21:13 +0000

    first commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 213124
+let accountName = "bhushan"
+var accountPassword = "21321321"
+city = "nagpur"
+let abc
+// accountId = 23213
+console.log(accountId)
+
+console.table([accountId, accountName, accountPassword, city, abc])
